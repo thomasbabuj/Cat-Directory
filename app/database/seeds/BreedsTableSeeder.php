a150e1e63d402108353843e25dadbb6dfbcb95bc
Thomas Babu <tommy.developer@gmail.com>
2014-08-15 15:37:51 +0800

    added migration and seeder class

---
+++ app/database/seeds/BreedsTableSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+* BreedsTable Seeder class
+*
+ */
+
+class BreedsTableSeeder extends Seeder {
+  public function run() {
+    DB::table('breeds')->insert(array(
+      array('id' => 1, 'name' => 'Domestic'),
+      array('id' => 2, 'name' => 'Persian'),
+      array('id' => 3, 'name' => 'Slamese'),
+      array('id' => 4, 'name' => 'Abyssinian'),
+    ));
+  }
+}
