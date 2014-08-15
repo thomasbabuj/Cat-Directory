7f6b3436e8cf50e21d66f6670fe5e6ab61b55ffe
Thomas Babu <tommy.developer@gmail.com>
2014-08-15 15:49:58 +0800

    added master view tempalte

---
+++ app/views/master.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Cats DB</title>
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}" />
+  </head>
+  <body>
+    <div class="container">
+      <div class="page-header">
+        @yield('header')
+      </div>
+      @if(Session::has('message'))
+        <div class="alert alert-success">
+          {{ Session::get('message') }}
+        </div>
+      @endif
+
+      @yield('content')
+    </div>
+  </body>
+</html>
