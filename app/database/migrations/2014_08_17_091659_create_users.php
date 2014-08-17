55e05a61673ac5aa60a4f1cd015a38c9c62681a4
Thomas Babu <tommy.developer@gmail.com>
2014-08-17 17:23:05 +0800

    created users migration

---
+++ app/database/migrations/2014_08_17_091659_create_users.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsers extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		//
+		Schema::create('users', function($table){
+			$table->increments('id');
+			$table->string('username');
+			$table->string('password');
+			$table->boolean('is_admin');
+			$table->timestamps();
+		});
+
+		Schema::table('cats', function($table){
+			$table->integer('user_id')->nullable()->references('id')->on('users');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+		Schema::table('cats', function($table){
+			$table->dropForeign('cats_user_id_foreign');
+			$table->dropColumn('user_id');
+		});
+
+		Schema::drop('users');
+	}
+
+}
