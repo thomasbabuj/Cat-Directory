0900bced2686520791fe45806a2fc5eeaebf965a
Thomas Babu <tommy.developer@gmail.com>
2014-08-17 17:26:31 +0800

    created users seeder class

---
+++ app/database/seeds/UsersTableSeeder.php
@@ -0,0 +1,13 @@
+<?php
+
+class UserTableSeeder extends Seeder {
+	public function run()
+	{
+		 DB::table('users')->truncate();
+
+		 DB::table('users')->insert(array(
+		 	array('username' => 'admin', 'password' => Hash::make('hunter2'), 'is_admin' => true ),
+		 	array('username' => 'scott', 'password' => Hash::make('tigher'), 'is_admin' => false )
+		 ));
+	}
+}
