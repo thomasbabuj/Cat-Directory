0b02291ad3899d565dd3dd126e7192978dcff163
Thomas Babu <tommy.developer@gmail.com>
2014-08-18 00:40:48 +0800

    updating master template with Auth options

--- app/views/master.blade.php
+++ app/views/master.blade.php
@@ -8,11 +8,24 @@
   <body>
     <div class="container">
       <div class="page-header">
+        @if ( Auth::check() )
+          Logged in as 
+          <strong>{{{ Auth::user()->username }}}</strong>
+          {{ link_to('logout', 'Log Out') }}
+        @else
+          {{ link_to('login', 'Log In') }}
+        @endif
         @yield('header')
       </div>
-      @if(Session::has('message'))
+      @if( Session::has('message') )
         <div class="alert alert-success">
           {{ Session::get('message') }}
+        </div>
+      @endif
+
+      @if( Session::has('error') )
+        <div class="alert alert-warning">
+          {{ Session::get('error') }}
         </div>
       @endif
 
