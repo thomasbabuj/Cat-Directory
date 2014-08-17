75a3d4c91b063b0e3a0ceb084b086b66763c64f0
Thomas Babu <tommy.developer@gmail.com>
2014-08-17 17:29:43 +0800

    fixed the breeds migration integrity constraint violation bug

--- app/database/seeds/BreedsTableSeeder.php
+++ app/database/seeds/BreedsTableSeeder.php
@@ -6,12 +6,18 @@
  */
 
 class BreedsTableSeeder extends Seeder {
+  
   public function run() {
+  	
+  	DB::table('users')->truncate();
+
     DB::table('breeds')->insert(array(
       array('id' => 1, 'name' => 'Domestic'),
       array('id' => 2, 'name' => 'Persian'),
       array('id' => 3, 'name' => 'Slamese'),
       array('id' => 4, 'name' => 'Abyssinian'),
     ));
+    
   }
+
 }
